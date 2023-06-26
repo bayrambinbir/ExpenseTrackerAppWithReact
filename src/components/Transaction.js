8147cf441cea1714c9c6530573e72068df366869
Bayram Binbir <bayrambinbir@Bayrams-MacBook-Pro.local>
2023-06-26 07:02:09 -0500

    expense tracker app with react

---
+++ src/components/Transaction.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+function Transaction({ transaction }) {
+  const { deleteTransaction } = useContext(GlobalContext);
+  
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const plusOrMinues = transaction.amount < 0 ? "minus" : "plus";
+
+  return (
+    <li className={plusOrMinues}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
+    </li>
+  );
+}
+
+export default Transaction;
